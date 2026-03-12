ff8856ac78731440b0fcffe560b5535a1469fffa
Mohammad <mohammad70623@gmail.com>
2026-03-13 02:30:22 +0600

    imported all necessery libraries

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-def main():
-    print("Hello from langgraph!")
+import random
+from fastmcp import FastMCP 
 
+mcp = FastMCP(name="Demo Server")
+
+@mcp.tool
+def roll_dice(n_dice: int =1)->list[int]:
+    """ Roll N dice 6-slided dice and return the result """
+    return [random.randint(1,6) for _ in range(n_dice)]
+
+@mcp.tool
+def add_numbers(a:float, b:float)-> float:
+    """ Add two numbers together"""
+    return a+b
 
 if __name__ == "__main__":
-    main()
+    mcp.run()
+
+
+
+
+
